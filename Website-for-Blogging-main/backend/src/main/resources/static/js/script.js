07a394360ced4682e23b66d67195f96c4ddd2d8d
dineshkr01 <aec.cse.dinesh@gmail.com>
2025-11-02 14:06:54 +0530

    Delete Website-for-Blogging-main directory

--- Website-for-Blogging-main/backend/src/main/resources/static/js/script.js
+++
@@ -1,7 +0,0 @@
-console.log("Page Loaded");
-const readblog = document.getElementById('readblogbutton');
-readblog.addEventListener("click", function () {
-    console.log("Hello World");
-    // window.location.href = "blogPage.html";
-});
-
